4cc064434441ce42661f7b18082f5e8be85279d8
hendisantika <hendisantika@yahoo.co.id>
2024-05-31 08:46:12 +0700

    Add CORSFilter🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/webfluxjwt/security/AuthenticationManager.java
+++ src/main/java/id/my/hendisantika/webfluxjwt/security/AuthenticationManager.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
---
+++ src/main/java/id/my/hendisantika/webfluxjwt/security/CORSFilter.java
@@ -0,0 +1,26 @@
+package id.my.hendisantika.webfluxjwt.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-webflux-jwt
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/31/24
+ * Time: 08:45
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@EnableWebFlux
+public class CORSFilter implements WebFluxConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
+    }
+}
