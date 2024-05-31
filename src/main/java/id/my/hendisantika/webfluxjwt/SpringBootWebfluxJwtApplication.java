9935fbbbe004fbd354ae6f43373e25d3e81c81ee
hendisantika <hendisantika@yahoo.co.id>
2024-05-31 08:34:10 +0700

    Create Spring Boot App 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/webfluxjwt/SpringBootWebfluxJwtApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.webfluxjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebfluxJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebfluxJwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/webfluxjwt/SpringBootWebfluxJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.webfluxjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebfluxJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
