669c07bfd66a520dea26c3b6d781be608610a385
hendisantika <hendisantika@yahoo.co.id>
2024-05-31 09:49:21 +0700

    Add UserService🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/webfluxjwt/service/UserService.java
@@ -0,0 +1,22 @@
+package id.my.hendisantika.webfluxjwt.service;
+
+import id.my.hendisantika.webfluxjwt.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-webflux-jwt
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/31/24
+ * Time: 09:49
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+public class UserService {
+
+    private Map<String, User> data;
+}
