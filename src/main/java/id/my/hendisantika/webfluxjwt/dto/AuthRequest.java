77fc0220d32908606d027c7eb403aa5ec007b967
hendisantika <hendisantika@yahoo.co.id>
2024-05-31 08:40:12 +0700

    Add AuthRequest DTO🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/webfluxjwt/dto/AuthRequest.java
@@ -0,0 +1,23 @@
+package id.my.hendisantika.webfluxjwt.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-webflux-jwt
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/31/24
+ * Time: 08:39
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthRequest {
+    private String username;
+    private String password;
+}
