7e45893e3a08bfb4fdc3f6a2fd5b21f32c0d4316
hendisantika <hendisantika@yahoo.co.id>
2024-05-31 08:39:21 +0700

    Add Role Model🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/webfluxjwt/model/Role.java
@@ -0,0 +1,15 @@
+package id.my.hendisantika.webfluxjwt.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-webflux-jwt
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/31/24
+ * Time: 08:39
+ * To change this template use File | Settings | File Templates.
+ */
+public enum Role {
+    ROLE_USER, ROLE_ADMIN
+}
