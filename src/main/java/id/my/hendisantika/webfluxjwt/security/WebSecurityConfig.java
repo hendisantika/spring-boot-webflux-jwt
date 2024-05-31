c2275e248924763c361598a2db45b61c3f4ac53b
hendisantika <hendisantika@yahoo.co.id>
2024-05-31 09:48:27 +0700

    Add securityWebFilterChain in WebSecurityConfig🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/webfluxjwt/security/WebSecurityConfig.java
+++ src/main/java/id/my/hendisantika/webfluxjwt/security/WebSecurityConfig.java
@@ -1,8 +1,14 @@
 package id.my.hendisantika.webfluxjwt.security;
 
 import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +27,26 @@ public class WebSecurityConfig {
 
     private AuthenticationManager authenticationManager;
     private SecurityContextRepository securityContextRepository;
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .exceptionHandling(
+                        handling -> handling
+                                .authenticationEntryPoint((swe, e) ->
+                                        Mono.fromRunnable(() -> swe.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED))
+                                ).accessDeniedHandler((swe, e) ->
+                                        Mono.fromRunnable(() -> swe.getResponse().setStatusCode(HttpStatus.FORBIDDEN))
+
+                                ))
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
+                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
+                .authenticationManager(authenticationManager)
+                .securityContextRepository(securityContextRepository)
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers(HttpMethod.OPTIONS).permitAll()
+                        .pathMatchers("/login").permitAll()
+                        .anyExchange().authenticated()).build();
+    }
 }
