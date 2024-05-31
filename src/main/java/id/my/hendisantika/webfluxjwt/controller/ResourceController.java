ca65d3dd45a029cbd457ad8165d684649d9dcb61
hendisantika <hendisantika@yahoo.co.id>
2024-05-31 09:51:49 +0700

    Add ResourceController🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/webfluxjwt/controller/ResourceController.java
@@ -0,0 +1,17 @@
+package id.my.hendisantika.webfluxjwt.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-webflux-jwt
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/31/24
+ * Time: 09:51
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+public class ResourceController {
+}
