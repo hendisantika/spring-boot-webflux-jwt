81038db77a63a38c56fcbd7f7c369ebad3e28052
hendisantika <hendisantika@yahoo.co.id>
2024-05-31 09:51:21 +0700

    Add login in AuthenticationController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/webfluxjwt/controller/AuthenticationController.java
+++ src/main/java/id/my/hendisantika/webfluxjwt/controller/AuthenticationController.java
@@ -1,11 +1,18 @@
 package id.my.hendisantika.webfluxjwt.controller;
 
+import id.my.hendisantika.webfluxjwt.dto.AuthRequest;
+import id.my.hendisantika.webfluxjwt.dto.AuthResponse;
 import id.my.hendisantika.webfluxjwt.security.JWTUtil;
 import id.my.hendisantika.webfluxjwt.security.PBKDF2Encoder;
 import id.my.hendisantika.webfluxjwt.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 /**
  * Created by IntelliJ IDEA.
@@ -24,4 +31,12 @@ public class AuthenticationController {
     private final JWTUtil jwtUtil;
     private final PBKDF2Encoder passwordEncoder;
     private final UserService userService;
+
+    @PostMapping("/login")
+    public Mono<ResponseEntity<AuthResponse>> login(@RequestBody AuthRequest ar) {
+        return userService.findByUsername(ar.getUsername())
+                .filter(userDetails -> passwordEncoder.encode(ar.getPassword()).equals(userDetails.getPassword()))
+                .map(userDetails -> ResponseEntity.ok(new AuthResponse(jwtUtil.generateToken(userDetails))))
+                .switchIfEmpty(Mono.just(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build()));
+    }
 }
