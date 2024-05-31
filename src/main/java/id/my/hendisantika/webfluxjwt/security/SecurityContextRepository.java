6e31ed61c2d94e8d8ae01b09ce6076cfbba54341
hendisantika <hendisantika@yahoo.co.id>
2024-05-31 08:48:23 +0700

    Add load in SecurityContextRepository🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/webfluxjwt/security/SecurityContextRepository.java
+++ src/main/java/id/my/hendisantika/webfluxjwt/security/SecurityContextRepository.java
@@ -1,6 +1,10 @@
 package id.my.hendisantika.webfluxjwt.security;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextImpl;
 import org.springframework.security.web.server.context.ServerSecurityContextRepository;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
@@ -28,5 +32,16 @@ public class SecurityContextRepository implements ServerSecurityContextRepositor
     @Override
     public Mono<Void> save(ServerWebExchange swe, SecurityContext sc) {
         throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Mono<SecurityContext> load(ServerWebExchange swe) {
+        return Mono.justOrEmpty(swe.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION))
+                .filter(authHeader -> authHeader.startsWith("Bearer "))
+                .flatMap(authHeader -> {
+                    String authToken = authHeader.substring(7);
+                    Authentication auth = new UsernamePasswordAuthenticationToken(authToken, authToken);
+                    return this.authenticationManager.authenticate(auth).map(SecurityContextImpl::new);
+                });
     }
 }
