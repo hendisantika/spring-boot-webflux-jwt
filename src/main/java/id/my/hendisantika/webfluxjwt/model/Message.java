1d128bd7aa7730489ad52d3f3ae8f4f573d80acb
hendisantika <hendisantika@yahoo.co.id>
2024-05-31 08:39:41 +0700

    Add Message Model🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/webfluxjwt/model/Message.java
@@ -0,0 +1,22 @@
+package id.my.hendisantika.webfluxjwt.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-webflux-jwt
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/31/24
+ * Time: 08:39
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Message {
+    private String content;
+}
