c936f58f72f8b60603a8be98fb51aa76b6f5b1b9
hendisantika <hendisantika@yahoo.co.id>
2024-05-31 08:47:06 +0700

    Add encode in PBKDF2Encoder🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/webfluxjwt/security/PBKDF2Encoder.java
+++ src/main/java/id/my/hendisantika/webfluxjwt/security/PBKDF2Encoder.java
@@ -4,6 +4,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Base64;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : spring-boot-webflux-jwt
@@ -24,6 +30,23 @@ public class PBKDF2Encoder implements PasswordEncoder {
     private Integer iteration;
 
     @Value("${app.password.encoder.keylength}")
-    private Integer keylength;
+    private Integer keyLength;
 
+    /**
+     * More info (https://www.owasp.org/index.php/Hashing_Java) 404 :(
+     *
+     * @param cs password
+     * @return encoded password
+     */
+    @Override
+    public String encode(CharSequence cs) {
+        try {
+            byte[] result = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512")
+                    .generateSecret(new PBEKeySpec(cs.toString().toCharArray(), secret.getBytes(), iteration, keyLength))
+                    .getEncoded();
+            return Base64.getEncoder().encodeToString(result);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
 }
