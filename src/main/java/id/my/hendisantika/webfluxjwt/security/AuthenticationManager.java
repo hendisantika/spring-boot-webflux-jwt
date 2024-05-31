c9cbb44878c950525e0e82c9569c5165794d2859
hendisantika <hendisantika@yahoo.co.id>
2024-05-31 08:42:04 +0700

    Add authenticate in AuthenticationManager🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/webfluxjwt/security/AuthenticationManager.java
+++ src/main/java/id/my/hendisantika/webfluxjwt/security/AuthenticationManager.java
@@ -1,8 +1,15 @@
 package id.my.hendisantika.webfluxjwt.security;
 
+import io.jsonwebtoken.Claims;
 import lombok.AllArgsConstructor;
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,4 +26,23 @@ import org.springframework.stereotype.Component;
 public class AuthenticationManager implements ReactiveAuthenticationManager {
 
     private JWTUtil jwtUtil;
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public Mono<Authentication> authenticate(Authentication authentication) {
+        String authToken = authentication.getCredentials().toString();
+        String username = jwtUtil.getUsernameFromToken(authToken);
+        return Mono.just(jwtUtil.validateToken(authToken))
+                .filter(valid -> valid)
+                .switchIfEmpty(Mono.empty())
+                .map(valid -> {
+                    Claims claims = jwtUtil.getAllClaimsFromToken(authToken);
+                    List<String> rolesMap = claims.get("role", List.class);
+                    return new UsernamePasswordAuthenticationToken(
+                            username,
+                            null,
+                            rolesMap.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList())
+                    );
+                });
+    }
 }
