1b438b48ddc2f477fc6003a13c8e14d7ada8d3e2
hendisantika <hendisantika@yahoo.co.id>
2024-05-31 09:52:31 +0700

    Add user in ResourceController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/webfluxjwt/controller/ResourceController.java
+++ src/main/java/id/my/hendisantika/webfluxjwt/controller/ResourceController.java
@@ -1,6 +1,13 @@
 package id.my.hendisantika.webfluxjwt.controller;
 
+import id.my.hendisantika.webfluxjwt.model.Message;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 /**
  * Created by IntelliJ IDEA.
@@ -12,6 +19,13 @@ import org.springframework.web.bind.annotation.RestController;
  * Time: 09:51
  * To change this template use File | Settings | File Templates.
  */
+@Slf4j
 @RestController
+@RequestMapping("/resource")
 public class ResourceController {
+    @GetMapping("/user")
+    @PreAuthorize("hasRole('USER')")
+    public Mono<ResponseEntity<Message>> user() {
+        return Mono.just(ResponseEntity.ok(new Message("Content for user")));
+    }
 }
